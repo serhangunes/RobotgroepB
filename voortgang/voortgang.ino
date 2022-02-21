780ed471f3d339fca8da66a0cf73fe64fc5cfcf7
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-02-21 14:53:04 +0100

    voortgang 21-2-2022

---
+++ voortgang/voortgang.ino
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <analogWrite.h>
+#include <Adafruit_VL53L0X.h>
+
+int motorPinRA = 16; //Rechterwiel achteruit
+int motorPinRV = 17; //Rechterwiel vooruit
+int motorPinLV = 5; //Linkerwiel vooruit
+int motorPinLA = 18; //Linkerwiel achteruit
+//int analogPin1 = 39;//
+//int analogPin2 = 34;//
+int pin1 = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(motorPinRA, OUTPUT);
+  pinMode(motorPinRV, OUTPUT);
+  pinMode(motorPinLV, OUTPUT);
+  pinMode(motorPinLA, OUTPUT);
+}
+
+void loop(){
+
+//aflezen
+pin1 = analogRead(12);
+Serial.println(pin1);
+
+//
+standStill();
+delay(3000);
+turnLeft();
+delay(340);
+standStill();
+  
+
+}
+
+/*
+ * functions
+ */
+
+/*
+ * Stand still
+ */
+void standStill() {
+  analogWrite(motorPinRA, 0);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, 0);
+}
+
+/*
+ * Drive Forward
+ */
+void driveForward() {
+  analogWrite(motorPinRA, 0);
+  analogWrite(motorPinRV, 233);
+  analogWrite(motorPinLV, 254);
+  analogWrite(motorPinLA, 0);
+}
+
+void turnLeft() {
+  analogWrite(motorPinRA, 0);
+  analogWrite(motorPinRV, 233);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, 0);
+}
