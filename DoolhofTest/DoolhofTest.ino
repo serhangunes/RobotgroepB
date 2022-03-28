3d1204dcd372561fb960ea0db104ebe2aab9c8ee
FrancaBaars <franca.baars@student.nhlstenden.com>
2022-03-28 14:55:46 +0200

    doolhof test bestand gemaakt

--- DoolhofTest/DoolhofTest.ino
+++ DoolhofTest/DoolhofTest.ino
@@ -6,12 +6,11 @@
 #include <Adafruit_SSD1306.h>  
 #include "WiFi.h"
 
-
 //declaring the pins
 int motorPinLV = 16;
 int motorPinLA = 17;
-int motorPinRA = 18;
-int motorPinRV = 5;
+int motorPinRV = 18;
+int motorPinRA = 5;
 int IRPinR = 39;
 int IRPinL = 34;
 
@@ -25,15 +24,9 @@ Adafruit_SSD1306 display(128, 64, &Wire, 4);
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
-/*
- * --------------------------------------------------------------
- * -------------------------Setup--------------------------------
- * --------------------------------------------------------------
- */
-
 void setup() {
-  //setup display
-if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+//setup display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
@@ -47,30 +40,20 @@ if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
   display.setTextColor(SSD1306_WHITE);
   display.display();
 
-//setup pins
+  //setup pins
   Serial.begin(115200);
   pinMode(motorPinRA, OUTPUT);
   pinMode(motorPinRV, OUTPUT);
   pinMode(motorPinLV, OUTPUT);
   pinMode(motorPinLA, OUTPUT);
+
 }
 
-/*
- * --------------------------------------------------------------
- * -------------------------Loop---------------------------------
- * --------------------------------------------------------------
- */
 void loop() {
-  // read the IR sensor
-pinL = analogRead(IRPinL);
-pinR = analogRead(IRPinR);
-detectColours();
-
+readPins();
 /*
  * display
  */
-  VL53L0X_RangingMeasurementData_t measure;
-  lox.rangingTest(&measure, false);
 //clear the display
   display.clearDisplay();
 
@@ -80,87 +63,70 @@ detectColours();
   display.println(colourL);
   display.print("PinR: ");
   display.println(colourR);
-  display.print("Dist: ");
-  display.println(measure.RangeMilliMeter);
   display.display();
 
-/*
- * maze
- */
-  analogWrite(motorPinRA, 0);
-  analogWrite(motorPinRV, 0);
-  analogWrite(motorPinLV, 0);
-  analogWrite(motorPinLA, 0);
-
-//if(pinL < white && pinR < white){
-//    driveForward(100);
-//}else if(pinL > white && pinL < grey && pinR < white){
-//    turnLeft(100);
-//}else if(pinL < white && pinR > white && pinR < grey) {
-//  turnRight(100);
-//}else{
-//  standStill();
-//}
-// if(pinL < white && pinR < white)  
-// {
-//    driveForward(70);
-// }
-// else if(pinL < white && pinR > white && pinR < black) 
-// {
-//    turnRight(70);
-// }
-// else if(pinL < white && pinR > black)  
-// {
-//    turnRight90();
-//    standStill();
-//    if(pinL > black && pinR > black)  
-//    {
-//        turn90Backwards();
-//        driveForward(70);
-//    }
-//    else if(pinL < black && pinR > black)  
-//    {
-//        driveForward(70);
-//    }
-// }
-// else if(pinL > white && pinL < black && pinR < white) 
-// {
-//    turnLeft(70);
-// }
-// else if(pinL > white && pinL < black && pinR > white && pinR < black) 
-// {
-//    turn180();
-// }
-// else if(pinL > white && pinL < black && pinR > black) 
-// {
-//    turnRight90();
-//    standStill();
-//    if(pinL > black && pinR > black)  
-//    {
-//      turn90Backwards();
-//      driveForward(70);
-//    }
-//    else if(pinL < black && pinR > black)  
-//    {
-//      driveForward(70);
-//    }
-// }
-// else if(pinL > black && pinR < white) 
-// {
-//    turnLeft90();
-// }
-// else if(pinL > black && pinR > white && pinR < black) 
-// {
-//    turnLeft90();
-// }
-// else if(pinL > black && pinR > black) 
-// {
-//    turnLeft90();
-// }
-
-
- 
+//If both are white then drive forward
+if(colourL == "white" && colourR == "white")
+{
+  driveForward(100);
 }
+//if right is grey then adjust to the right.
+else if(colourL == "white" && colourR == "grey")
+{
+  turnRight(100);
+}
+//if right is black then turn 90 degrees right and if there is a road ahead.
+else if(colourL == "white" && colourR == "black")
+{
+  fancy function
+}
+//if left is grey then adjust to the left.
+else if(colourL == "grey" && colourR == "white")
+{
+  turnLeft(100);
+}
+//if both are grey then turn 180 degrees.
+else if(colourL == "grey" && colourR == "grey")
+{
+  turn180();
+}
+//if right is black then turn right and check if there is a road ahead.
+else if(colourL == "grey" && colourR == "black")
+{
+  fancy function
+}
+//If left is black then turn 90 degrees to the left.
+if(colourL == "black" && colourR == "white")
+{
+  turnLeft90();
+}
+//if left is black then turn 90 degrees to the left.
+if(colourL == "black" && colourR == "grey")
+{
+  turnLeft90();
+}
+//if both are black then turn 90 degrees to the left.
+else if(colourL == "black" && colourR == "black")
+{
+  turnLeft90();
+}
+else
+{
+  standStill();
+}
+//  if(colourL == "white" && colourR == "white")  {
+//    driveForward(100);
+//  }else if(colourL == "grey" && colourR == "white") {
+//    turnLeft(100);
+//  }else if(colourL == "white" && colourR == "grey") {
+//    turnRight(100);
+//  }else if(colourL == "grey" && colourR == "grey")  {
+//    standStill();
+//  }else{
+//    standStill();
+//  }
+}
+
 
 /*
  * --------------------------------------------------------------
@@ -168,13 +134,16 @@ detectColours();
  * --------------------------------------------------------------
  */
 
-/*
- * Detect colours
+ /*
+ * Read the pins and give a colour
  */
-void detectColours()  {
-  if(pinR <= 80)  {
+void readPins() {
+pinL = analogRead(IRPinL);
+pinR = analogRead(IRPinR);
+
+  if(pinR <= 85)  {
     colourR = "white";
-  }else if(pinR > 80 && pinR <=1100)  {
+  }else if(pinR > 85 && pinR <=1100)  {
     colourR = "grey";
   }else if(pinR > 1100) {
     colourR = "black";
@@ -189,13 +158,9 @@ void detectColours()  {
   }
 }
 
- /*
-  * Drive forward
-  */
-
-void driveForward(double percentage)  {
-  int speedR = int((170.0f / 100.0f) * percentage);
-  int speedL = int((170.0f / 100.0f) * percentage);
+void driveForward(double percentage) {
+  int speedR = int((255.0f / 100.0f) * percentage);
+  int speedL = int((255.0f / 100.0f) * percentage);
   
   analogWrite(motorPinRA, 0);
   analogWrite(motorPinRV, speedR);
@@ -220,11 +185,11 @@ void standStill() {
 
 void turnLeft(double percentage) {
   int speedR = int((255.0f / 100.0f) * percentage);
-  int speedL = int((225.0f / 100.0f) * percentage);
+  int speedL = int((200.0f / 100.0f) * percentage);
   
   analogWrite(motorPinRA, 0);
-  analogWrite(motorPinRV, 200);
-  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, speedL);
   analogWrite(motorPinLA, 0);
 }
 
@@ -233,12 +198,12 @@ void turnLeft(double percentage) {
  */
 
 void turnRight(double percentage) {
-  int speedR = int((255.0f / 100.0f) * percentage);
-  int speedL = int((225.0f / 100.0f) * percentage);
+  int speedR = int((200.0f / 100.0f) * percentage);
+  int speedL = int((255.0f / 100.0f) * percentage);
   
   analogWrite(motorPinRA, 0);
-  analogWrite(motorPinRV, 0);
-  analogWrite(motorPinLV, 200);
+  analogWrite(motorPinRV, speedR);
+  analogWrite(motorPinLV, 0);
   analogWrite(motorPinLA, 0);
 }
 
@@ -261,8 +226,8 @@ void turnLeft90() {
 void turnRight90()  {
   analogWrite(motorPinRA, 0);
   analogWrite(motorPinRV, 0);
-  analogWrite(motorPinLV, 0);
-  analogWrite(motorPinLA, 170);
+  analogWrite(motorPinLV, 255);
+  analogWrite(motorPinLA, 0);
   delay(500);
 }
 
@@ -274,10 +239,10 @@ void driveBackwards(double percentage){
   int speedR = int((255.0f / 100.0f) * percentage);
   int speedL = int((225.0f / 100.0f) * percentage);
   
-  analogWrite(motorPinRA, 0);
-  analogWrite(motorPinRV, speedR);
-  analogWrite(motorPinLV, speedL);
-  analogWrite(motorPinLA, 0);
+  analogWrite(motorPinRA, speedR);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, speedL);
 }
 
 /*
@@ -296,8 +261,8 @@ void turn180()  {
  * turn 90 backwards
  */
 void turn90Backwards()  {
-  analogWrite(motorPinRA, 0);
-  analogWrite(motorPinRV, 255);
+  analogWrite(motorPinRA, 255);
+  analogWrite(motorPinRV, 0);
   analogWrite(motorPinLV, 0);
   analogWrite(motorPinLA, 0);
   delay(500);
