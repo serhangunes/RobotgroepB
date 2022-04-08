620ed12343eabc5db91a640482214007b451ee80
Yannieck Blaauw <yannieck.blaauw@student.nhlstenden.com>
2022-04-08 10:46:12 +0200

    Butler comments toegevoegd

--- DefinitieveProgramma/DefinitieveProgramma/butlerspelen.h
+++ DefinitieveProgramma/DefinitieveProgramma/butlerspelen.h
@@ -1,6 +1,7 @@
 bool canCheck = true;
 int valCap = 50;
 
+//Function for measuring the distance from the LiDAR sensor
 float measure() {
   VL53L0X_RangingMeasurementData_t measure;
   lidar.rangingTest(&measure, false);
@@ -8,9 +9,13 @@ float measure() {
   return dist;
 }
 
+//Execute this function when the robot needs to look for the hole in the wall
 void lookForHole() {
   float dist = measure();
   canCheck = false;
+  
+  //Turn a bit to the left, then start turning right.
+  //When the sensor gets above a certain threshold, move forwards.
   turnLeft(60);
   delay(400);
 
@@ -27,6 +32,7 @@ void lookForHole() {
   canCheck = true;
 }
 
+//This is the loop for the butlergame
 void butlerLoop() {
   VL53L0X_RangingMeasurementData_t measure;
   lidar.rangingTest(&measure, false);
@@ -35,6 +41,8 @@ void butlerLoop() {
   if (measure.RangeStatus != 4) {
     writeToDisplay("d: " + String(dist), 0, 0);
     writeToDisplay("L: " + String(IRValL) + " R: " + String(IRValR), 0, 1);
+    
+    //When the distance is under a certain threshold, start looking for the hole in the wall
     if (dist <= 30.0f && canCheck == true) {
       standStill();
       lookForHole();
@@ -42,6 +50,7 @@ void butlerLoop() {
       driveForward(80);
     }
   }
+  //When the bot sees tape op the ground, finish the game
   if (IRValL >= 2000 && IRValR >= 2000) {
     botStatus = "finished";
   }
